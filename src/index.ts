89ec59678b02ec4a579be9bf62adae386cbeb7c1
Mizan-Rifat <mizanrifat001@gmail.com>
2025-01-11 00:45:51 +0600

    redis added

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@ import 'dotenv/config';
 import bodyParser from 'body-parser';
 import routes from './routes';
 import { DatabaseService } from './services/database.service';
+import { RedisService } from './services/redis.service';
 
 const app = express();
 const PORT = process.env.APP_PORT || 3000;
 
 const dbService = DatabaseService.getInstance();
+const redisService = RedisService.getInstance();
 
 (async () => {
   try {
     await dbService.connect();
+    await redisService.connect();
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,10 +1,19 @@
 import { DatabaseService } from '../../services/database.service';
+import { RedisService } from '../../services/redis.service';
 
 export class UserService {
   private dbService = DatabaseService.getInstance();
+  private redisService = RedisService.getInstance();
 
   async getAllUsers() {
     const db = this.dbService.getDb();
+    const redisClient = this.redisService.getClient();
+
+    // Check if the data is already in the cache
+    const cachedData = await redisClient.get('name');
+    if (cachedData) {
+      return cachedData;
+    }
     return await db.collection('users').find().toArray();
   }
 }
